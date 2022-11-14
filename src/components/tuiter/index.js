70b8306288f428bffa5824f9f0935c3d799c0bfe
nala-he <hytdaisy@gmail.com>
2022-11-14 11:10:38 -0500

    added restaurant path navigating to BusinessHome page in tuiter/index.js

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Navigation from "../navigation";
 import WhatsHappening from "../whats-happening";
-import {BrowserRouter, Routes, Route, HashRouter} from "react-router-dom";
+import {Routes, Route, HashRouter} from "react-router-dom";
 import Home from "../home";
 import Bookmarks from "../bookmarks";
 import Profile from "../profile";
@@ -15,6 +15,7 @@ import More from "../more";
 import {Login} from "../profile/login";
 import Movies from "../movies";
 import MovieDetails from "../movies/details";
+import BusinessHome from "../restaurant/business-home";
 
 function Tuiter () {
   return(
@@ -41,6 +42,7 @@ function Tuiter () {
               <Route path="/profile/edit" element={<EditProfile/>}/>
               <Route path="/movies" element={<Movies/>}/>
               <Route path="/movies/:imdbID" element={<MovieDetails/>}/>
+              <Route path="/restaurant" element={<BusinessHome/>}/>
               <Route path="/more" element={<More/>}/>
             </Routes>
           </div>
