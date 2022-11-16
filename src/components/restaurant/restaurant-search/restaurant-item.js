7797a9d18c78be1c64861cd3832a8d1637f7c517
Macee Qi <maceeqi@Macees-MacBook-Pro-2.local>
2022-11-16 14:46:42 -0500

    finished implementing search bar and restaurant post component

--- src/components/restaurant/restaurant-search/restaurant-item.js
+++ src/components/restaurant/restaurant-search/restaurant-item.js
@@ -6,11 +6,12 @@ const RestaurantItem = ({
                                 _id: "123",
                                 name: "Red Lobster",
                                 handle: "@redlobster",
+                                profilePicture: "redlobster-avatar.png",
                                 bio: "The best red lobster. Come and taste!",
                                 cuisine: "Seafood",
                                 price: "$$",
                                 address: "Silver Spring, MD",
-                                open: {
+                                hours: {
                                     monday: "11:00AM - 10:00 PM",
                                     tuesday: "11:00AM - 10:00 PM",
                                     wednesday: "Closed",
@@ -26,7 +27,36 @@ const RestaurantItem = ({
 ) => {
     return (
         <div className="ttr-restaurant-post-border mb-3 p-3">
-            {restaurant.name}
+            <div className="d-flex">
+                <div>
+                    <img src={`/images/${restaurant.profilePicture}`}
+                         className="ttr-square-avatar"/>
+                </div>
+                <div className="ms-3 me-1 w-50">
+                    <span className="fw-bold">{restaurant.name}</span><br/>
+                    {restaurant.handle}<br/>
+                    <p className="mt-2 mb-0">
+                        {restaurant.bio}
+                    </p>
+                </div>
+                <div className="ms-3">
+                    {restaurant.cuisine} | {restaurant.price}<br/>
+                    {restaurant.address}<br/>
+                    {restaurant.phone}<br/>
+                    {restaurant.website}<br/>
+                    {/*<span>*/}
+                    {/*    Mon: {restaurant.hours.monday}<br/>*/}
+                    {/*    Tues: {restaurant.hours.tuesday}<br/>*/}
+                    {/*    Wed: {restaurant.hours.wednesday}<br/>*/}
+                    {/*    Thurs: {restaurant.hours.thursday}<br/>*/}
+                    {/*    Fri: {restaurant.hours.friday}<br/>*/}
+                    {/*    Sat: {restaurant.hours.saturday}<br/>*/}
+                    {/*    Sun: {restaurant.hours.sunday}<br/>*/}
+                    {/*</span>*/}
+                </div>
+            </div>
+
+
         </div>
     );
 };
