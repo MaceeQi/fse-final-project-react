6bef00568d43b290dccd67dfd11b2941901d3484
Macee Qi <maceeqi@Macees-MacBook-Pro-2.local>
2022-11-16 15:24:08 -0500

    removed hours from restaurant item

--- src/components/restaurant/restaurant-search/restaurant-item.js
+++ src/components/restaurant/restaurant-search/restaurant-item.js
@@ -11,15 +11,6 @@ const RestaurantItem = ({
                                 cuisine: "Seafood",
                                 price: "$$",
                                 address: "Silver Spring, MD",
-                                hours: {
-                                    monday: "11:00AM - 10:00 PM",
-                                    tuesday: "11:00AM - 10:00 PM",
-                                    wednesday: "Closed",
-                                    thursday: "11:00AM - 10:00 PM",
-                                    friday: "11:00AM - 10:00 PM",
-                                    saturday: "11:00AM - 10:00 PM",
-                                    sunday: "11:00AM - 10:00 PM"
-                                },
                                 phone: "617-234-5678",
                                 website: "www.delicious.com"
                             }
@@ -44,15 +35,6 @@ const RestaurantItem = ({
                     {restaurant.address}<br/>
                     {restaurant.phone}<br/>
                     {restaurant.website}<br/>
-                    {/*<span>*/}
-                    {/*    Mon: {restaurant.hours.monday}<br/>*/}
-                    {/*    Tues: {restaurant.hours.tuesday}<br/>*/}
-                    {/*    Wed: {restaurant.hours.wednesday}<br/>*/}
-                    {/*    Thurs: {restaurant.hours.thursday}<br/>*/}
-                    {/*    Fri: {restaurant.hours.friday}<br/>*/}
-                    {/*    Sat: {restaurant.hours.saturday}<br/>*/}
-                    {/*    Sun: {restaurant.hours.sunday}<br/>*/}
-                    {/*</span>*/}
                 </div>
             </div>
 
