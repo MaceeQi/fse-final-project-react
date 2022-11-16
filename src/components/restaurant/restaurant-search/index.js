3b8f93f461e7c919d9eaaf390c2870f25185f344
Macee Qi <maceeqi@Macees-MacBook-Pro-2.local>
2022-11-16 13:06:17 -0500

    restaurant search bar

---
+++ src/components/restaurant/restaurant-search/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./index.css";
+
+const RestaurantSearch = () => {
+    return (
+        <div >
+            {/* search bar */}
+            <div className="row">
+                {/* input field */}
+                <div className="ttr-width-90">
+                    <div className="position-relative">
+                        <input className="ps-5 bg-secondary bg-opacity-10 border-0 form-control
+                                          form-control-lg rounded-pill"
+                               placeholder="Search Restaurants"/>
+                        <i className="position-absolute ttr-search bi bi-search"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default RestaurantSearch;
--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -15,6 +15,7 @@ import More from "../more";
 import {Login} from "../profile/login";
 import Movies from "../movies";
 import MovieDetails from "../movies/details";
+import RestaurantSearch from "../restaurant/restaurant-search"
 
 function Tuiter () {
   return(
@@ -41,6 +42,7 @@ function Tuiter () {
               <Route path="/profile/edit" element={<EditProfile/>}/>
               <Route path="/movies" element={<Movies/>}/>
               <Route path="/movies/:imdbID" element={<MovieDetails/>}/>
+              <Route path="/restaurant" element={<RestaurantSearch/>}/>
               <Route path="/more" element={<More/>}/>
             </Routes>
           </div>
