a3c18def3b1f27704bbe47edfd2e01c1f0b81b55
Chanon Bovornvirakit <bovornvirakit.c@northeastern.edu>
2022-11-27 19:28:46 -0500

    small changes to reviews-service.js

--- src/services/reviews-service.js
+++ src/services/reviews-service.js
@@ -2,25 +2,32 @@ import axios from "axios";
 const BASE_URL = "http://localhost:4000";
 
 const RESTAURANTS_API = `${BASE_URL}/api/restaurant`;
+const REVIEWS_API = `${BASE_URL}/api/reviews`;
 
-export const findAllReviews = (restaurantid) =>
+export const findAllReviews = () =>
+    axios.get(`${REVIEWS_API}`)
+        .then(response => response.data)
+
+export const findAllReviewsForRestaurant = (restaurantid) =>
     axios.get(`${RESTAURANTS_API}/${restaurantid}/reviews`)
+        .then(response => response.data);
+
+export const findAllReviewsByCritic = (criticid) =>
+    axios.get(`${BASE_URL}/users/${criticid}/reviews`)
+        .then(response => response.data);
+
+export const findReviewById = (reviewid) =>
+    axios.get(`${REVIEWS_API}/${reviewid}`)
         .then(response => response.data);
 
 export const createReview = (restaurantid, uid, review) =>
     axios.post(`${RESTAURANTS_API}/${restaurantid}/users/${uid}/reviews`, review)
         .then(response => response.data);
 
-export const updateReview = (restaurantid, uid, reviewid, review) =>
-    axios.put(`${RESTAURANTS_API}/${restaurantid}/users/${uid}/reviews/${reviewid}`, review)
+export const updateReview = (reviewid, review) =>
+    axios.put(`${REVIEWS_API}/${reviewid}`, review)
         .then(response => response.data);
 
-export const deleteReview = (restaurantid, uid, reviewid) =>
-    axios.delete(`${RESTAURANTS_API}/${restaurantid}/users/${uid}/reviews/${reviewid}`)
+export const deleteReview = (reviewid) =>
+    axios.delete(`${REVIEWS_API}/${reviewid}`)
         .then(response => response.data);
-
-const service = {
-    findAllReviews
-}
-
-export default service;
