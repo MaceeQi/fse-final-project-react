a5fea7cbc4885d20df5fe2802f84c4a7df1f56ce
Chanon Bovornvirakit <bovornvirakit.c@northeastern.edu>
2022-11-28 15:14:38 -0500

    added user credentials axios stuff

--- src/services/reviews-service.js
+++ src/services/reviews-service.js
@@ -4,6 +4,8 @@ const BASE_URL = "http://localhost:4000";
 const RESTAURANTS_API = `${BASE_URL}/api/restaurants`;
 const REVIEWS_API = `${BASE_URL}/api/reviews`;
 
+const api = axios.create({withCredentials: true});
+
 export const findAllReviews = () =>
     axios.get(`${REVIEWS_API}`)
         .then(response => response.data)
