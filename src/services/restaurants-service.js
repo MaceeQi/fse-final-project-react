6e8b24909e8017ccef22925dadc6c2a896fc2a27
MaceeQi <maceeqi@gmail.com>
2022-11-30 15:28:26 -0500

    restaurants service

---
+++ src/services/restaurants-service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:4000";
+
+const RESTAURANTS_API = `${BASE_URL}/api/restaurants`;
+
+export const findAllRestaurants = () =>
+    axios.get(`${RESTAURANTS_API}`)
+        .then(response => response.data);
+
+export const findRestaurantById = (rid) =>
+    axios.get(`${RESTAURANTS_API}/${rid}`)
+        .then(response => response.data);
+
+export const createRestaurant = (restaurant) =>
+    axios.post(`${RESTAURANTS_API}`, restaurant)
+        .then(response => response.data);
+
+export const updateRestaurant = (restaurant) =>
+    axios.put(`${RESTAURANTS_API}/${restaurant._id}`, restaurant)
+        .then(response => response.data);
+
+export const deleteRestaurant = (rid) =>
+    axios.delete(`${RESTAURANTS_API}/${rid}`)
+        .then(response => response.data);
+
+export const deleteRestaurantByRestaurantName = (name) =>
+    axios.delete(`${RESTAURANTS_API}/name/${name}/delete`)
+        .then(response => response.data);
+
+export const findRestaurantsByName = (name) =>
+    axios.get(`${RESTAURANTS_API}/name/${name}`)
+        .then(response => response.data);
