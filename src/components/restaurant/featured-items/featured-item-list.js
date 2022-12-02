ef7388238fa8306ca90aadaaefb5d98449df2380
nala-he <hytdaisy@gmail.com>
2022-12-01 22:40:30 -0500

    added useEffect and thunk function in featured-item-list.js

--- src/components/restaurant/featured-items/featured-item-list.js
+++ src/components/restaurant/featured-items/featured-item-list.js
@@ -1,8 +1,14 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {findFeaturedItemsByRestaurantThunk} from "../../../services/featured-item-thunks";
 
 const FeatureList = ({restaurant}) => {
     const features = useSelector(state => state.features);
+
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(findFeaturedItemsByRestaurantThunk(restaurant._id));
+    }, [])
 
     return (
         <div className="mb-3 border ttr-border-radius">
