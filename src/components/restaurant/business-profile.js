346f07df9121ea4466dab2a954d6363e96ce4c04
nala-he <hytdaisy@gmail.com>
2022-12-05 11:21:16 -0500

    followed business-home to update the fix on business-profile page as well

--- src/components/restaurant/business-profile.js
+++ src/components/restaurant/business-profile.js
@@ -36,33 +36,39 @@ const BusinessProfile = () => {
                 Loading...
             </h5>
         }
-        <div className="position-relative ttr-banner d-flex justify-content-center">
-            <img src={`/images/${restaurant.bannerPicture}`}
-                 alt="banner"
-                 className="ttr-border-radius ttr-banner-width mt-3" height={200}/>
-            <img className="ttr-portrait position-absolute start-0 ms-5"
-                 alt="profile"
-                 src={`/images/${restaurant.profilePicture}`}/>
-        </div>
-        <div className="m-3 position-relative">
+        {
+            restaurant &&
+            <>
+                <div className="position-relative ttr-banner d-flex justify-content-center">
+                    <img src={`/images/${restaurant.bannerPicture}`}
+                         alt="banner"
+                         className="ttr-border-radius ttr-banner-width mt-3" height={200}/>
+                    <img className="ttr-portrait position-absolute start-0 ms-5"
+                         alt="profile"
+                         src={`/images/${restaurant.profilePicture}`}/>
+                </div>
+                <div className="m-3 position-relative">
                 <span className="h5 fw-bolder">{restaurant.name} {restaurant.handle}
                 </span><br/>
-            {/*Profile edit button*/}
-            <Link to="./edit">
-                <button className="btn btn-white border rounded-pill fw-bolder
-            float-end me-3 mt-2">Edit</button>
-            </Link>
-            <p className="mt-3 mb-3">{restaurant.bio}</p>
-            <BusinessInfo restaurant={restaurant}/>
-            {/*<UpdateList restaurant={restaurant}/>*/}
-            <FeatureList/>
-            <div className="mb-3 border ttr-border-radius">
-                <div className="m-2">
-                    <h5 className="fw-bolder">Professional Reviews</h5>
-                    <ReviewList restaurant={restaurant} critics={critics}/>
+                    {/*Profile edit button*/}
+                    <Link to="./edit">
+                        <button className="btn btn-white border rounded-pill fw-bolder
+            float-end me-3 mt-2">Edit
+                        </button>
+                    </Link>
+                    <p className="mt-3 mb-3">{restaurant.bio}</p>
+                    <BusinessInfo restaurant={restaurant}/>
+                    {/*<UpdateList restaurant={restaurant}/>*/}
+                    <FeatureList/>
+                    <div className="mb-3 border ttr-border-radius">
+                        <div className="m-2">
+                            <h5 className="fw-bolder">Professional Reviews</h5>
+                            <ReviewList restaurant={restaurant} critics={critics}/>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            </>
+        }
     </div>
     );
 }
