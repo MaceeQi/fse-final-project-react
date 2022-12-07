615269a7c31e76678c21b744aadda2225ad04d04
Chanon Bovornvirakit <bovornvirakit.c@northeastern.edu>
2022-12-07 13:11:49 -0500

    more test stuff

--- src/tests/reviews-service.test.js
+++ src/tests/reviews-service.test.js
@@ -104,3 +104,56 @@ describe('can delete review with REST API', () => {
         expect(status.deletedCount).toEqual(1);
     })
 });
+
+describe('can retrieve all reviews with REST API', () => {
+    const testCritic = {
+        username: "harshreviewer",
+        password: "reviews123uwu",
+        email: "restaurant@critics.com"
+    }
+    const testReview = {
+        review: "this place sucks!",
+        restaurant: "637c182a59bca90266c414a6"
+    }
+    const testReview2 = {
+        review: "this place is great!",
+        restaurant: "637c182a59bca90266c414a6"
+    }
+
+    const testCritic2 = {
+        username: "boringreviewer",
+        password: "reviews123owo",
+        email: "restaurant@observers.com"
+    }
+    const testReview3 = {
+        review: "no comment",
+        restaurant: "637c182a59bca90266c414a6"
+    }
+    let newCritic;
+    let newCritic2;
+    beforeAll(async () => {
+        newCritic = await createUser(testCritic);
+        await createReview(newCritic.id, testReview.restaurant, testReview);
+        await createReview(newCritic.id, testReview2.restaurant, testReview2);
+
+        newCritic2 = await createUser(testCritic2);
+        await createReview(newCritic2.id, testReview3.restaurant, testReview3);
+    })
+    afterAll(async () => {
+        const reviews = await findAllReviewsByCritic(newCritic.id);
+        for (let each of reviews) {
+            await deleteReview(each._id)
+        }
+        const reviews2 = await findAllReviewsByCritic(newCritic2.id);
+        for (let each of reviews2) {
+            await deleteReview(each._id)
+        }
+        await deleteUsersByUsername(newCritic.username)
+        await deleteUsersByUsername(newCritic2.username)
+    })
+
+    test('findAllReviews', async () => {
+        const reviews = await findAllReviews();
+        expect(reviews.length).toEqual(3);
+    })
+});
