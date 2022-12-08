6a6600d3686b16cb99479573a31325ae36c3567c
bovornc <113566236+bovornc@users.noreply.github.com>
2022-12-07 19:02:39 -0500

    reverted reviews-thunks

--- src/services/reviews-thunks.js
+++ src/services/reviews-thunks.js
@@ -22,8 +22,9 @@ export const findReviewByIdThunk = createAsyncThunk(
 )
 
 export const createReviewThunk = createAsyncThunk(
-    'reviews/createReview', async ({newReview}) =>
-        await reviewsService.createReview({newReview})
+    'reviews/createReview', async (review) =>
+        await reviewsService.createReview(review.criticid, review.restaurantid, review)
+        // await reviewsService.createReview("6383e8fde3994dcd7623e825", "637c1ccb59bca90266c414a7", review)
 )
 
 export const updateReviewThunk = createAsyncThunk(
