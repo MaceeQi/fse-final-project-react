e3d60f798a89619fb20161ce0a39616305c6158c
nala-he <hytdaisy@gmail.com>
2022-12-08 23:00:35 -0500

    finished restaurant service testing

--- src/services/restaurants-service.js
+++ src/services/restaurants-service.js
@@ -3,6 +3,8 @@ const BASE_URL = "http://localhost:4000";
 
 const RESTAURANTS_API = `${BASE_URL}/api/restaurants`;
 
+axios.defaults.adapter = require('axios/lib/adapters/http')
+
 export const findAllRestaurants = () =>
     axios.get(`${RESTAURANTS_API}`)
         .then(response => response.data);
@@ -17,7 +19,7 @@ export const findRestaurantById = async (rid) => {
 
 export const createRestaurant = async (restaurant) => {
     const response = await axios.post(`${RESTAURANTS_API}`, restaurant);
-    console.log(response.data);
+    // console.log(response.data);
     return response.data;
 }
 
@@ -35,11 +37,15 @@ export const deleteRestaurant = (rid) =>
         .then(response => response.data);
 
 
-export const deleteRestaurantByRestaurantName = (name) =>
-    axios.delete(`${RESTAURANTS_API}/name/${name}/delete`)
-        .then(response => response.data);
+// export const deleteRestaurantByRestaurantName = (name) =>
+//     axios.delete(`${RESTAURANTS_API}/name/${name}/delete`)
+//         .then(response => response.data);
 
 
 export const findRestaurantsByName = (name) =>
     axios.get(`${RESTAURANTS_API}/name/${name}`)
+        .then(response => response.data);
+
+export const deleteRestaurantByOwner = (owner) =>
+    axios.delete(`${RESTAURANTS_API}/users/${owner}`)
         .then(response => response.data);
--- src/services/users-service.js
+++ src/services/users-service.js
@@ -4,6 +4,8 @@ const BASE_URL = "http://localhost:4000";
 const LOGIN_API = `${BASE_URL}/api/login`;
 const USERS_API = `${BASE_URL}/api/users`;
 
+axios.defaults.adapter = require('axios/lib/adapters/http')
+
 export const createUser = (user) =>
   axios.post(`${USERS_API}`, user)
     .then(response => response.data);
